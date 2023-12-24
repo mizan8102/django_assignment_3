d23b04601d9f098aed6746190b405ff5ac276b3f
Mizanur Rahman <mizan.prog@gmail.com>
2023-12-24 16:03:29 +0600

    Feature: adding meal, about pages

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def about_us(request):
+    return render(request, "About_us.html")
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('meal.urls'))
+    path("admin/", admin.site.urls),
+    path("", include("meal.urls")),
+    path("", include("about_us.urls")),
 ]
